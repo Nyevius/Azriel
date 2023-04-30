bc66932e00e3beee8db5810bd9d75d8a5eb0f0b1
Glitch (neat-curious-baker) <none>
2023-04-30 14:25:27 +0000

    Updated with Glitch

--- commands/stats/me.js
+++ commands/stats/me.js
@@ -1,85 +1,56 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed } = require("discord.js");
 const Database = require('../../Helpers/Database');
-const vt = new Database('Database', 'Voice');
-const mdb = new Database('Database', 'Message');
-const moment = require('moment');
-require('moment-duration-format');
+const vt = new Database("Database", "Voice");
+const mdb = new Database("Database", "Message");
+const moment = require("moment");
+require("moment-duration-format");
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('me')
-    .setDescription('Provides information about your statistics on the server.'),
-  async execute(interaction) {
-    const member = interaction.member;
-    const voiceData = vt.get(`stats.${interaction.guild.id}.${member.id}`) || {
-      voice: 0,
-      channels: {},
-    };
-    const messageData = mdb.get(`stats.${interaction.guild.id}.${member.id}`) || {
-      messages: 0,
-      channels: {},
-    };
+    data: {
+        name: "me",
+        description: "Provides information about your statistics on the server."
+    },
+    async execute(interaction) {
+        let voiceData = vt.get(`stats.${interaction.guildId}.${interaction.user.id}`) || {voice: 0, channels: {}};
+        let messageData = mdb.get(`stats.${interaction.guildId}.${interaction.user.id}`) || {messages: 0, channels: {}};
 
-    let voiceList = Object.keys(voiceData.channels)
-      .map((vd) => {
-        return {
-          Id: vd,
-          Total: voiceData.channels[vd],
-        };
-      })
-      .sort((a, b) => b.Total - a.Total);
+        let voiceList = Object.keys(voiceData.channels).map(vd => {
+            return {
+                Id: vd,
+                Total: voiceData.channels[vd]
+            };
+        }).sort((a, b) => b.Total - a.Total);
 
-    let messageList = Object.keys(messageData.channels)
-      .map((md) => {
-        return {
-          Id: md,
-          Total: messageData.channels[md],
-        };
-      })
-      .sort((a, b) => b.Total - a.Total);
+        let messageList = Object.keys(messageData.channels).map(md => {
+            return {
+                Id: md,
+                Total: messageData.channels[md]
+            };
+        }).sort((a, b) => b.Total - a.Total);
 
-    voiceList = voiceList.length > 10 ? voiceList.splice(0, 10) : voiceList;
-    voiceList = voiceList
-      .map(
-        (vd, index) =>
-          `\`${index + 1}.\` ${
-            interaction.client.channels.cache.has(vd.Id)
-              ? interaction.client.channels.cache.get(vd.Id).toString()
-              : '#deleted-channel'
-          }: \`${moment.duration(vd.Total).format('H [hours,] m [minutes]')}\``
-      )
-      .join('\n');
-    messageList = messageList.length > 10 ? messageList.splice(0, 10) : messageList;
-    messageList = messageList
-      .map(
-        (md, index) =>
-          `\`${index + 1}.\` ${
-            interaction.client.channels.cache.has(md.Id)
-              ? interaction.client.channels.cache.get(md.Id).toString()
-              : '#deleted-channel'
-          }: \`${md.Total} message\``
-      )
-      .join('\n');
-    const embed = new MessageEmbed()
-      .setColor(member.displayHexColor)
-      .setFooter(`${member.user.tag} | Powered by Serendia Squad`)
-      .setThumbnail(member.user.avatarURL({ dynamic: true }))
-      .addField(
-        'User Information',
-        `\`ID:\` ${member.id}\n\`Roles:\` ${
-          member.roles.cache.size >= 5 ? 'Roles are too much...' : member.roles.cache.map((role) => role.toString())
-        }\n\`Nickname:\` ${member.displayName}`
-      )
-      .addField(
-        'Voice Activity',
-        `Last Activity: ${new Date(voiceData.activity).toLocaleDateString()}\n** **${voiceList}`
-      )
-      .addField(
-        'Message Activity',
-        `Last Activity: ${new Date(messageData.activity).toLocaleDateString()}\n** **${messageList}`
-      );
+        voiceList = voiceList.length > 10 ? voiceList.splice(0, 10) : voiceList;
+        voiceList = voiceList.map((vd, index)=> `\`${index + 1}.\` ${interaction.client.channels.cache.has(vd.Id) ? interaction.client.channels.cache.get(vd.Id).toString() : "#deleted-channel"}: \`${moment.duration(vd.Total).format("H [hours,] m [minutes]")}\``).join("\n");
+        messageList = messageList.length > 10 ? messageList.splice(0, 10) : messageList;
+        messageList = messageList.map((md, index)=> `\`${index + 1}.\` ${interaction.client.channels.cache.has(md.Id) ? interaction.client.channels.cache.get(md.Id).toString() : "#deleted-channel"}: \`${md.Total} message\``).join("\n");
+        let embed = new MessageEmbed();
+        embed.setColor(interaction.member.displayHexColor)
+        .setFooter(`${interaction.user.tag} | Powered by Serendia Squad`)
+        .setThumbnail(interaction.user.avatarURL({dynamic: true}))
+        .addField("User Information",` 
+    
+        \`ID:\` ${interaction.user.id} 
+        \`Roles:\` ${interaction.member.roles.cache.size >= 5 ? "Roles are too much..." : interaction.member.roles.cache.map(role => role.toString())}
+        \`Nickname:\` ${interaction.member.displayName}
+        `)
+        .addField("Voice Activity", `
+        Last Activity: ${new Date(voiceData.activity).toLocaleDateString()}
+        ** **${voiceList}
+        `)
+        .addField("Message Activity", `
+        Last Activity: ${new Date(messageData.activity).toLocaleDateString()}
+        ** **${messageList}
+        `);
 
-    interaction.reply({ embeds: [embed] });
-  },
+        await interaction.reply({ embeds: [embed] });
+    },
 };
